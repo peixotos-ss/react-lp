0e3f82634e105a260aa7740a8465c885e55745e4
peixotos-ss <brunolorenzet02@gmail.com>
2026-05-20 12:43:09 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import s from "./App.module.css";
+import img1 from '/01.jpg';
+import img2 from '/02.jpg'; 
+import img3 from '/03.jpg'; 
+
+
+function App() {
+  return (
+    <>
+      <nav>
+        
+        <span className={s.navitems}>
+          <i class="fa-solid fa-house"></i>
+          <a className={s.navwords} href="#s1">
+            BMW
+          </a>
+        </span>
+
+        <span className={s.navitems}>
+          <i class="fa-solid fa-car"></i>
+          <a className={s.navwords} href="#s2">
+            Models
+          </a>
+        </span>
+
+        <span className={s.navitems}>
+          <i class="fa-solid fa-video"></i>
+          <a className={s.navwords} href="#s3">
+            Videos
+          </a>
+        </span>
+
+        <span className={s.navitems}>
+          <i class="fa-solid fa-address-book"></i>
+          <a className={s.navwords} href="#s4">
+            Contact
+          </a>
+        </span>
+      </nav>
+
+      <main>
+        <section className={s.s1} id="s1">
+          <h1>BMW</h1>
+          <h2>The balance of power and luxury</h2>
+        </section>
+
+        <section className={s.s2} id="s2">
+          <div className={s.card}>
+            <img src={img1} alt="BMW M3" />
+            <div>
+              <h4>BMW M3 Competition</h4>
+              <p>O ícone da performance em sua forma mais pura.</p>
+            </div>
+          </div>
+
+          <div className={s.card}>
+          
+            <img src={img2} alt="BMW Série 5" />
+            <div>
+              <h4>BMW Série 5 Sedã</h4>
+              <p>A nova definição do luxo executivo.</p>
+            </div>
+          </div>
+
+          <div className={s.card}>
+          
+            <img src={img3} alt="BMW X3" />
+            <div>
+              <h4>BMW X3 M50 xDrive</h4>
+              <p>O SUV esportivo de nova geração.</p>
+            </div>
+          </div>
+        </section>
+
+        <section className={s.s3} id="s3">
+          <iframe
+            width="640"
+            height="480"
+            src="https://www.youtube.com/embed/vLy4l9lSBMQ"
+            title="BMW"
+            allowFullScreen
+          ></iframe>
+        </section>
+
+        <section className={s.s4} id="s4">
+          <h3>Contato</h3>
+          <div className={s.formulario}>
+            <label>Nome:</label>
+            <input type="text" placeholder="Digite seu nome" />
+            <label>Mensagem</label>
+            <textarea placeholder="Digite sua mensagem"></textarea>
+            <button>ENVIAR</button>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <a href="https://youtube.com" target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-youtube"></i>
+        </a>
+      </footer>
+    </>
+  );
+}
+
+export default App;
